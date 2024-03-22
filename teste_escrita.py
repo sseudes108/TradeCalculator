2c43c4a0cbf88b614bd6483228ac1197d2c2a78a
Basilísco <senstur.art@gmail.com>
2024-03-22 00:02:38 -0300

    teste linux server

--- teste_escrita.py
+++ teste_escrita.py
@@ -1,15 +1,14 @@
 import subprocess
 
 arquivo = ["services", "passwd"]
-palavras = ["ssh", "3398"]
+palavras = ["ssh", "3389", "22", "80"]
+
+comando = "cat /etc/passwd | grep {} > file.txt".format("eudes")
+subprocess.run(comando, shell=True)
 
 for palavra in palavras:
-    comando = "cat /etc/{} | grep {} > file.txt".format(arquivo[0], palavra)
+    comando = "cat /etc/{} | grep {} >> file.txt".format(arquivo[0], palavra)
     subprocess.run(comando, shell=True)
 
-comando = "cat /etc/{} | grep {} > file2.txt".format(arquivo[1], "eudes")
-subprocess.run(comando, shell=True)
 
-comando = "cat file.txt | grep >> file2.txt"
-subprocess.run(comando, shell=True)
-
+print("Arquivo file.txt criado")
