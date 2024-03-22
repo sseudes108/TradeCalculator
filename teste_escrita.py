c8a1e07a26ad73de822d87f6ab00797102764bfc
Basilísco <senstur.art@gmail.com>
2024-03-21 23:50:13 -0300

    teste linux server

---
+++ teste_escrita.py
@@ -0,0 +1,15 @@
+import subprocess
+
+arquivo = ["services", "psswd"]
+palavras = ["ssh", "3398"]
+
+for palavra in palavras:
+    comando = "cat /etc/{} | grep {} > file.txt".format(arquivo[0], palavra)
+    subprocess.run(comando, shell=True)
+
+comando = "cat /etc/{} | grep {} > file2.txt".format(arquivo[1], "eudes")
+subprocess.run(comando, shell=True)
+
+comando = "cat file.txt | grep {} >> file2.txt".format(arquivo[1], "eudes")
+subprocess.run(comando, shell=True)
+
