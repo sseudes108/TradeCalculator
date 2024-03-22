99780f10425889245577e0e283258a799d92f060
Basilísco <senstur.art@gmail.com>
2024-03-22 17:55:42 -0300

    teste log linux server

--- linux_server/cron_teste.py
+++ linux_server/cron_teste.py
@@ -1,21 +1,20 @@
 import subprocess
 import os
 
-
 def criar_log():
     home_dir = os.path.expanduser("~")
-    log_path = os.path.join(home_dir, "logs")
+    log_path = os.path.join(home_dir, "logs/")
     log = ler_log(log_path) + 1
 
     with open("{}log.txt".format(log_path), "w", encoding='utf-8') as log_file:
         log_file.write("{}".format(log, end=""))
 
-    with open("{}log{}.txt".format(log_path, log), "w", encoding='uft-8') as new_log_file:
+    with open("{}log{}.txt".format(log_path, log), "w", encoding='utf-8') as new_log_file:
         new_log_file.write("{}".format(log, end=""))
 
-    processo = "cat /var/log/syslog | tail -n 10 > {}log{}.txt".format(log_path, log)
-    subprocess.run(processo, shell=True)
-
+    processo = ["tail", "-n", "10", "/var/log/syslog"]
+    with open("{}log{}.txt".format(log_path, log), "w", encoding='utf-8') as new_log_file:
+        subprocess.run(processo, stdout=new_log_file)
 
 def ler_log(log_path):
     log_file = open("{}log.txt".format(log_path), "r")
